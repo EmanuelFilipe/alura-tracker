4c034f23629fe0d72dbf1010438a4d8212657d60
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2022-01-19 18:22:03 -0300

    trabalhando com listas

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Box",
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,35 @@
+<template>
+  <Box class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Box from "./Box.vue";
+import Cronometro from "./Cronometro.vue";
+import ITarefa from "../interfaces/ITarefa";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: {
+    Cronometro,
+    Box,
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>

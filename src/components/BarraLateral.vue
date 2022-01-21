8e62302a1914835d452a40c00464024a2c1dbcba
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2022-01-21 07:03:14 -0300

    implementando o gerenciamento de estado

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,7 +3,23 @@
     <h1>
       <img src="../assets/logo.png" alt="" />
     </h1>
-    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <div class="has-text-centered">
+      <button @click="alterarTema">{{ textoBotao }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"> tarefas</i>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"> projetos</i>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -35,14 +51,26 @@ export default defineComponent({
 
 <style scoped>
 header {
-  padding: 1rem;
+  padding: 2rem;
   background: #0d3b66;
   widows: 100%;
   height: 100vh;
-  text-align: center;
 }
 
-.button {
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #faf0ca;
+}
+
+.link.router-link-active {
+  color: #faf0ca;
 }
 
 @media only screen and (max-width: 768px) {
